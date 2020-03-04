8c42ba72dd2907bed01be29fbc3480fe7e158c1a
Roman Marchenko <roman.marchenko@saltpepper.ru>
2020-03-04 10:46:01 +0300

    Add move/touch gestures

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,13 @@ export interface EventsArguments {
     loaded: number,
     total: number
   ) => void;
+  onMouseDown?: (e: MouseEvent, ctx: Context) => void;
+  onMouseMove?: (e: MouseEvent, ctx: Context) => void;
+  onMouseUp?: (e: MouseEvent, ctx: Context) => void;
+  onTouchStart?: (e: TouchEvent, ctx: Context) => void;
+  onTouchEnd?: (e: TouchEvent, ctx: Context) => void;
+  onTouchCancel?: (e: TouchEvent, ctx: Context) => void;
+  onTouchMove?: (e: TouchEvent, ctx: Context) => void;
 }
 
 interface ContextArguments {
@@ -57,7 +64,7 @@ const useThree = (
   const lastTime = useRef<number>(0);
   const callbacks = useRef<EventsArguments>(events);
 
-  const createRenderer = () => {
+  const createRenderer = useCallback(() => {
     if (context.webgl2) {
       const canvas = document.createElement("canvas");
       const canvasContext = canvas.getContext("webgl2");
@@ -69,7 +76,57 @@ const useThree = (
     } else {
       return new THREE.WebGLRenderer();
     }
-  };
+  }, []);
+
+  const createEventListener = useCallback((event, handler) => {
+    window.addEventListener(event, handler);
+
+    return () => {
+      window.removeEventListener(event, handler);
+    };
+  }, []);
+
+  const handleMouseDown = useCallback(e => {
+    if (callbacks.current.onMouseDown) {
+      callbacks.current.onMouseDown(e, storage.current);
+    }
+  }, []);
+
+  const handleMouseMove = useCallback(e => {
+    if (callbacks.current.onMouseMove) {
+      callbacks.current.onMouseMove(e, storage.current);
+    }
+  }, []);
+
+  const handleMouseUp = useCallback(e => {
+    if (callbacks.current.onMouseUp) {
+      callbacks.current.onMouseUp(e, storage.current);
+    }
+  }, []);
+
+  const handleTouchStart = useCallback(e => {
+    if (callbacks.current.onTouchStart) {
+      callbacks.current.onTouchStart(e, storage.current);
+    }
+  }, []);
+
+  const handleTouchEnd = useCallback(e => {
+    if (callbacks.current.onTouchEnd) {
+      callbacks.current.onTouchEnd(e, storage.current);
+    }
+  }, []);
+
+  const handleTouchCancel = useCallback(e => {
+    if (callbacks.current.onTouchCancel) {
+      callbacks.current.onTouchCancel(e, storage.current);
+    }
+  }, []);
+
+  const handleTouchMove = useCallback(e => {
+    if (callbacks.current.onTouchMove) {
+      callbacks.current.onTouchMove(e, storage.current);
+    }
+  }, []);
 
   useEffect(() => {
     callbacks.current = events;
@@ -94,6 +151,41 @@ const useThree = (
 
       renderer.current.setSize(width, height);
       renderer.current.setPixelRatio(window.devicePixelRatio || 1);
+
+      const removeMouseDownListener = createEventListener(
+        "mousedown",
+        handleMouseDown
+      );
+
+      const removeMouseMoveListener = createEventListener(
+        "mousemove",
+        handleMouseMove
+      );
+
+      const removeMouseUpListener = createEventListener(
+        "mouseup",
+        handleMouseUp
+      );
+
+      const removeTouchStartListener = createEventListener(
+        "touchstart",
+        handleTouchStart
+      );
+
+      const removeTouchEndListener = createEventListener(
+        "touchend",
+        handleTouchEnd
+      );
+
+      const removeTouchCancelListener = createEventListener(
+        "touchcancel",
+        handleTouchCancel
+      );
+
+      const removeTouchMoveListener = createEventListener(
+        "touchmove",
+        handleTouchMove
+      );
 
       manager.current.onProgress = (
         item: any,
@@ -129,6 +221,13 @@ const useThree = (
       window.addEventListener("resize", resize, false);
 
       return () => {
+        removeMouseDownListener();
+        removeMouseMoveListener();
+        removeMouseUpListener();
+        removeTouchStartListener();
+        removeTouchEndListener();
+        removeTouchCancelListener();
+        removeTouchMoveListener();
         stop();
       };
     }
